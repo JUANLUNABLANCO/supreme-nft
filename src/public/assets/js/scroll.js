a8f779d37957796108d194918e4cefc4474afea2
gotth3way <gotth3way.apis@gmail.com>
2022-03-01 22:34:02 +0100

    revertir cambios en iconos

---
+++ src/public/assets/js/scroll.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+	let count = 0;
+	let posterVideo = $('#posterVideo');
+	let clipVideo = $('#clipVideo');
+
+	let widthDevice = window.innerWidth;
+
+	window.onscroll = function () {
+		var y = window.scrollY;
+		console.log(y);
+		if (widthDevice <= 740) {
+			if (y < 230 && count == 0) {
+				posterVideo.addClass('none');
+				clipVideo.addClass('none');
+				count++;
+			} else if (y > 224 && count > 0) {
+				posterVideo.removeClass('none');
+				clipVideo.removeClass('none');
+				count = 0;
+			}
+		}
+		if (widthDevice > 740 && widthDevice <= 1000) {
+			if (y < 125 && count == 0) {
+				posterVideo.addClass('none');
+				clipVideo.addClass('none');
+				count++;
+			} else if (y > 125 && count > 0) {
+				posterVideo.removeClass('none');
+				clipVideo.removeClass('none');
+				count = 0;
+			}
+		}
+	};
+});
