bce6f8e95fe6ab1b6faf35dc5c1ecc64b65b1d6c
JuanLuna <gotth3way.apis@gmail.com>
2022-11-10 13:13:19 +0100

    cambiado el .env

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 require('dotenv').config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT  || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
